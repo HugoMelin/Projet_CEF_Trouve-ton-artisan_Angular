e9fc724a415e2cff2fe8dffbf04070876a08dbd3
HugoMelin <hugo.melin01@gmail.com>
2024-06-14 15:24:57 +0200

    Add component titreh1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Titreh1Component } from './components/titres/titreh1/titreh1.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Titreh1Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

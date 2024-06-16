6eb205eea7f50b32624cbc215ec2cb1c52e81283
HugoMelin <hugo.melin01@gmail.com>
2024-06-16 15:52:40 +0200

    Add font awesome

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [ RouterLink ],
+  imports: [ RouterLink, FontAwesomeModule ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
 }

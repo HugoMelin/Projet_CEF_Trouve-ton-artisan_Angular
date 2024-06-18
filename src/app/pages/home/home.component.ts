b4b35d228941eb5c594659d20737919a45237d75
HugoMelin <hugo.melin01@gmail.com>
2024-06-18 18:56:35 +0200

    Add component howToUse

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HowToUseComponent } from '../../components/how-to-use/how-to-use.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ HowToUseComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })

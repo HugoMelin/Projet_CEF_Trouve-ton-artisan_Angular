8d890c10bb3894ae3baa2deb01e299f1ff8607e7
HugoMelin <hugo.melin01@gmail.com>
2024-06-20 14:58:14 +0200

    Add routing for artisans page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ArtisansComponent } from './pages/artisans/artisans.component';
 
 export const routes: Routes = [
-    {path: "", component: HomeComponent, title: "Accueil | Trouve ton artisan"}
+    {path: "", component: HomeComponent, title: "Accueil | Trouve ton artisan"},
+    {path: "artisans/:categorie", component: ArtisansComponent}
 ];
---
+++ src/app/pages/artisans/artisans.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {Title} from "@angular/platform-browser";
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-artisans',
+  standalone: true,
+  imports: [],
+  templateUrl: './artisans.component.html',
+  styleUrl: './artisans.component.scss'
+})
+export class ArtisansComponent implements OnInit {
+  categorie: string = ""
+
+  constructor(
+    private titleService:Title,
+    private route: ActivatedRoute)
+      {}
+  
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.categorie = params["categorie"];
+    })
+    this.titleService.setTitle(this.categorie) 
+  }
+}

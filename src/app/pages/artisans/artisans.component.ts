33937424e2920783f1fb630f5a46261f891592c7
HugoMelin <hugo.melin01@gmail.com>
2024-07-03 09:47:41 +0200

    Add search bar

--- src/app/pages/artisans/artisans.component.ts
+++ src/app/pages/artisans/artisans.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit } from '@angular/core';
 import {Title} from "@angular/platform-browser";
 import { ActivatedRoute } from '@angular/router';
 import { Titreh1Component } from '../../components/titres/titreh1/titreh1.component';
+import { Titreh2Component } from '../../components/titres/titreh2/titreh2.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-artisans',
   standalone: true,
-  imports: [Titreh1Component],
+  imports: [Titreh1Component, Titreh2Component, FontAwesomeModule],
   templateUrl: './artisans.component.html',
   styleUrl: './artisans.component.scss'
 })
 export class ArtisansComponent implements OnInit {
+  faMagnifyingGlass = faMagnifyingGlass
   categorie: string = ""
 
   constructor(

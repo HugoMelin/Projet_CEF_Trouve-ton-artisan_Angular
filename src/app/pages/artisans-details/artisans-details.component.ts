5fc8cd7f43dd6aafbe88758c388019faef97f6e7
Hugo Melin <hugo.melin01@gmail.com>
2024-07-06 18:07:18 +0200

    Create artisans-details component

---
+++ src/app/pages/artisans-details/artisans-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisans-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './artisans-details.component.html',
+  styleUrl: './artisans-details.component.scss'
+})
+export class ArtisansDetailsComponent {
+
+}

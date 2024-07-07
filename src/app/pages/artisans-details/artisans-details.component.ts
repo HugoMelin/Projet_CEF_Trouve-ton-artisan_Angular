bf2699f5c961dbe3ed5d25559d4e936291c8b585
Hugo Melin <hugo.melin01@gmail.com>
2024-07-07 22:17:51 +0200

    Add subrscition to get artisan data in artisan Detail page

--- src/app/pages/artisans-details/artisans-details.component.ts
+++ src/app/pages/artisans-details/artisans-details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArtisansDataService } from '../../services/artisans-data.service';
 
 @Component({
   selector: 'app-artisans-details',
@@ -8,5 +10,20 @@ import { Component } from '@angular/core';
   styleUrl: './artisans-details.component.scss'
 })
 export class ArtisansDetailsComponent {
+  routeSub:any
+  data:any
+  artisansData:any
 
+  constructor(
+    private route: ActivatedRoute,
+    private artisansDataService: ArtisansDataService
+  ) {}
+
+  ngOnInit() {
+    this.artisansData = this.artisansDataService.artisanList
+
+    this.routeSub = this.route.params.subscribe(params => {
+      this.data = this.artisansData.find((item: { id: any; }) => item.id === params['id'])
+    })
+  }
 }

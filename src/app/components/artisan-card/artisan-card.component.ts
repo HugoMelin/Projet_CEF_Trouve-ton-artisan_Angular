c78262d50e49288a14ff93bcecb63c32f818dbc4
Hugo Melin <hugo.melin01@gmail.com>
2024-07-07 22:33:45 +0200

    Add progressBar component

--- src/app/components/artisan-card/artisan-card.component.ts
+++ src/app/components/artisan-card/artisan-card.component.ts
@@ -2,11 +2,12 @@ import { NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ImagesArtisansComponent } from '../images-artisans/images-artisans.component';
 import { RouterLink } from '@angular/router';
+import { ProgressBarComponent } from '../progress-bar/progress-bar.component';
 
 @Component({
   selector: 'app-artisan-card',
   standalone: true,
-  imports: [NgSwitch, NgSwitchCase, ImagesArtisansComponent, RouterLink],
+  imports: [NgSwitch, NgSwitchCase, ImagesArtisansComponent, RouterLink, ProgressBarComponent],
   templateUrl: './artisan-card.component.html',
   styleUrl: './artisan-card.component.scss'
 })

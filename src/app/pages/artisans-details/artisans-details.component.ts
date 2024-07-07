3d582455b8ae1bd32d0222985feb9d6446d229cb
Hugo Melin <hugo.melin01@gmail.com>
2024-07-07 23:11:31 +0200

    Add content to artisans details component

--- src/app/pages/artisans-details/artisans-details.component.ts
+++ src/app/pages/artisans-details/artisans-details.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ArtisansDataService } from '../../services/artisans-data.service';
 import { Titreh1Component } from '../../components/titres/titreh1/titreh1.component';
+import { ProgressBarComponent } from '../../components/progress-bar/progress-bar.component';
+import { ImagesArtisansComponent } from '../../components/images-artisans/images-artisans.component';
+import { Titreh2Component } from '../../components/titres/titreh2/titreh2.component';
 
 @Component({
   selector: 'app-artisans-details',
   standalone: true,
-  imports: [Titreh1Component],
+  imports: [Titreh1Component, ProgressBarComponent, ImagesArtisansComponent, Titreh2Component],
   templateUrl: './artisans-details.component.html',
   styleUrl: './artisans-details.component.scss'
 })

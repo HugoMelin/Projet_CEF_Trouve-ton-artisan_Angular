24f6d9bc3cba902ffe19f6d1b08ae4c346f97f29
Hugo Melin <hugo.melin01@gmail.com>
2024-07-07 22:00:13 +0200

    Add routerLink on artisans card component

--- src/app/components/artisan-card/artisan-card.component.ts
+++ src/app/components/artisan-card/artisan-card.component.ts
@@ -1,11 +1,12 @@
 import { NgSwitch, NgSwitchCase } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ImagesArtisansComponent } from '../images-artisans/images-artisans.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-artisan-card',
   standalone: true,
-  imports: [NgSwitch, NgSwitchCase, ImagesArtisansComponent],
+  imports: [NgSwitch, NgSwitchCase, ImagesArtisansComponent, RouterLink],
   templateUrl: './artisan-card.component.html',
   styleUrl: './artisan-card.component.scss'
 })
--- src/app/components/artisans-du-mois/artisans-du-mois.component.ts
+++ src/app/components/artisans-du-mois/artisans-du-mois.component.ts
@@ -3,11 +3,12 @@ import { ArtisansDataService } from '../../services/artisans-data.service';
 import { Titreh2Component } from '../titres/titreh2/titreh2.component';
 import { NgFor } from '@angular/common';
 import { ArtisanDuMoisFilterPipe } from '../../pipes/artisan-du-mois-filter.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-artisans-du-mois',
   standalone: true,
-  imports: [ Titreh2Component, NgFor, ArtisanDuMoisFilterPipe ],
+  imports: [ Titreh2Component, NgFor, ArtisanDuMoisFilterPipe, RouterLink ],
   templateUrl: './artisans-du-mois.component.html',
   styleUrl: './artisans-du-mois.component.scss'
 })
